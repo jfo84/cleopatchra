bfab0d4e6d26883dc13703c5c3fa26548dee7bd9
Jack Ford <jford8820@gmail.com>
2017-10-03 16:57:28 +0200

    Add picojson header file. Add placeholder main.cpp. Run Ember CLI 2.15 init

---
+++ front_end/app/routes/index.js
@@ -0,0 +1,6 @@
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Route from '@ember/route';
+
+export default Route.extend({
+});
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "./lib/picojson.h"
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello world" << endl;
+  return 0;
+}

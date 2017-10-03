2deb71f5305fe595dad91a100bfdb0966b15e739
Jack Ford <jford8820@gmail.com>
2017-10-03 23:31:07 +0200

    Add function to connect to postgres. Need to figure out pull data structure and querys next

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <libpq-fe.h>
 #include "./lib/picojson.h"
 
 using namespace std;
 
 int main()
 {
-  cout << "Hello world" << endl;
+  PGconn *conn;
+
+  conn = connect("127.0.0.1", "5432", "cleopatchra");
+
+  if (NULL == conn) { return 1; }
+
+  // TODO: Figure out how to query
+
+  // query
+  PGresult *res = PQexec(conn, query);
+
   return 0;
+}
+
+PGconn *connect()
+{
+  const char *keys[] = {
+    "hostaddr", "port", "dbname", NULL
+  };
+
+  const char *values[] = {
+    hostaddr, port, dbname, NULL
+  };
+
+  PGconn *conn = PQconnectdbParams(keys, values, 0);
+  if (NULL == conn) { return NULL; }
+
+  ConnStatusType status = PQstatus(conn);
+
+  if (CONNECTION_BAD == status) {
+    fprintf(stderr, "error: %s\n", PQerrorMessage(conn));
+    PQfinish(conn);
+    return NULL;
+  }
+
+  return conn;
 }

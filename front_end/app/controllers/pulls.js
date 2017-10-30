df4879aed69e080b47b1b032d7a02231a92f4c50
Jack Ford <jford8820@gmail.com>
2017-10-30 14:47:26 +0100

    Add ember-css-modules. Rendering simple pull information for pulls route

--- front_end/app/controllers/pulls.js
+++ front_end/app/controllers/pulls.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 const { Controller, computed } = Ember;
 
 export default Controller.extend({
-  repos: computed.alias('model.pulls'),
+  pulls: computed.alias('model.pulls'),
 });

48d3f35580e8f70754bae9e9e9f4fdcff22aa6f0
Jack Ford <jford8820@gmail.com>
2017-11-03 11:27:50 +0100

    Add initial test with factories

---
+++ api/db/main_test.go
@@ -0,0 +1,102 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/bluele/factory-go/factory"
+	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
+)
+
+type Repo struct {
+	ID   int
+	Data string
+}
+
+type Pull struct {
+	ID   int
+	Data string
+	Repo *Repo
+}
+
+var PullFactory = factory.NewFactory(
+	&Pull{},
+).SeqInt("ID", func(n int) (interface{}, error) {
+	return n, nil
+}).Attr("Name", func(args factory.Args) (interface{}, error) {
+	pull := args.Instance().(*Pull)
+	return fmt.Sprintf("pull-%d", pull.ID), nil
+}).OnCreate(func(args factory.Args) error {
+	tx := args.Context().Value("tx").(*pg.Tx)
+	return tx.Insert(args.Instance())
+}).SubFactory("Repo", RepoFactory)
+
+var RepoFactory = factory.NewFactory(
+	&Repo{},
+).SeqInt("ID", func(n int) (interface{}, error) {
+	return n, nil
+}).Attr("Name", func(args factory.Args) (interface{}, error) {
+	repo := args.Instance().(*Repo)
+	return fmt.Sprintf("repo-%d", repo.ID), nil
+}).OnCreate(func(args factory.Args) error {
+	tx := args.Context().Value("tx").(*pg.Tx)
+	return tx.Insert(args.Instance())
+})
+
+func createTestSchema(db *pg.DB) error {
+	tables := []interface{}{
+		&Repo{},
+		&Pull{},
+	}
+	for _, table := range tables {
+		err := db.DropTable(table, &orm.DropTableOptions{
+			IfExists: true,
+			Cascade:  true,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = db.CreateTable(table, &orm.CreateTableOptions{
+			Temp: true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func openDB() *pg.DB {
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+
+	err := createTestSchema(db)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func TestDB(t *testing.T) {
+	db := openDB()
+	for i := 0; i < 3; i++ {
+		tx, err := db.Begin()
+		if err != nil {
+			panic(err)
+		}
+
+		ctx := context.WithValue(context.Background(), "tx", tx)
+		v, err := PullFactory.CreateWithContext(ctx)
+		if err != nil {
+			panic(err)
+		}
+		pull := v.(*Pull)
+		fmt.Println(pull, *pull.Repo)
+		tx.Commit()
+	}
+}

3814be425371fc89f3268d8b12b62929a642495a
Jack Ford <jford8820@gmail.com>
2017-11-11 15:34:00 +0000

    Fix typo

--- api/unmarshalling/main.go
+++ api/unmarshalling/main.go
@@ -6,7 +6,7 @@ import (
 
 /*
 This package exists for edge cases where the struct we want to build for exports does not
-fit cleanly into the json.Unmarshall interface.
+fit cleanly into the json.Unmarshal interface.
 
 With Pull, for example, we need to side-load comment internal ID's. However, the GitHub JSON
 contains a "comments" key with an int value (# of comments)

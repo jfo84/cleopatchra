5221b333cfc5e4f988f035fda5ab8d1f41562318
Jack Ford <jford8820@gmail.com>
2017-12-02 15:17:19 +0100

    Add initial reducer cruft for pulls index. Add initial App.jsx

--- front-end/reducer.js
+++ front-end/reducer.js
@@ -1,9 +1,33 @@
 import * as actionTypes from './actionTypes';
 
 const initialState = {
+  isFetching: false,
+  page: 1,
+  limit: 10
 };
 
 const reducer = (state = initialState, action) => {
+  const payload = action.payload;
+  
+    switch(action.type) {
+      case(actionTypes.REQUEST_PULLS):
+        return {
+          ...state,
+          ...payload
+        };
+      case(actionTypes.RECEIVE_PULLS):
+        return {
+          ...state,
+          ...payload
+        };
+      case(actionTypes.CHANGE_PAGE):
+        return {
+          ...state,
+          page: payload
+        };
+      default:
+        return state;
+    }
 }
 
 export default reducer;
